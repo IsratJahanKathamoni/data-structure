009c62169c8ca5e0994d5e187f0ded483408ba12
Israt Jahan kathamoni <104441853+IsratJahanKathamoni@users.noreply.github.com>
2023-03-08 19:37:55 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,15 +2,34 @@
 using namespace std;
 int main()
 {
-    int z;
-    char a[100];
-    cout<<" Enter the size of  array size : ";
+    int x,y,z;
+    cout<<"please inpute the size of array :";
     cin>>z;
-    for (int i = 0; i <z ; ++i)
-    {
-        cin>>a[i];
+    cout<<"Please inpute the range of array"<<'\n';
+    cout<<"please inpute the first number range  :";
+    cin>>y;
+    cout<<"Please inpute the last numbe of range :";
+    cin>>x;
+    int a[z];
+    for (int i = 0; i <z ; ++i) {
+        a[i]=rand()%(x-y)+y;
     }
     int temp;
+    for (int i = 0; i <z ; ++i) {
+        for (int j = i+1; j <z ; ++j) {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    cout<<'\n';
+    cout<<"DESCENDING ORDER : ";
+    for (int i = 0; i <z ; ++i) {
+        cout<<a[i]<<" ";
+    }
     for (int i = 0; i <z ; ++i) {
         for (int j = i+1; j <z ; ++j) {
             if(a[i]>a[j])
@@ -21,9 +40,11 @@ int main()
             }
         }
     }
-    for (int i = 0; i <z ; ++i)
-    {
-        cout<<a[i]<<" ";
+    cout<<'\n';
 
+    cout<<"ASCENDING ORDER : ";
+    for (int i = 0; i <z ; ++i) {
+        cout<<a[i]<<" ";
     }
+    return 0;
 }
