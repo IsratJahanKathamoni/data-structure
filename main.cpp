4578eb7f638089010d683f52ce34f34209e6ab2f
Israt Jahan kathamoni <104441853+IsratJahanKathamoni@users.noreply.github.com>
2023-03-08 19:42:04 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,50 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+void box_of_stars(int a, int h)
+{
+    for (int i = 1; i <a ; ++i) {
+        cout<<"*";
+    }
+    cout<<endl;
+    for(int b=1;b<=h;b++)
+    {
+        for(int c=1;c<=1;c++)
+        {
+            cout<<"*";
+        }
+        for(int d=1;d<=8;d++)
+        {
+            cout<<" ";
+        }
+        cout<<" * "<<endl;
+    }
+    for (int i = 1; i <a ; ++i) {
+        cout<<"*";
+    }
+    cout<<endl;
+}
 int main()
 {
-    int x,y,z;
-    cout<<"please inpute the size of array :";
-    cin>>z;
-    cout<<"Please inpute the range of array"<<'\n';
-    cout<<"please inpute the first number range  :";
-    cin>>y;
-    cout<<"Please inpute the last numbe of range :";
-    cin>>x;
-    int a[z];
-    for (int i = 0; i <z ; ++i) {
-        a[i]=rand()%(x-y)+y;
-    }
-    int temp;
-    for (int i = 0; i <z ; ++i) {
-        for (int j = i+1; j <z ; ++j) {
-            if(a[i]<a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-    cout<<'\n';
-    cout<<"DESCENDING ORDER : ";
-    for (int i = 0; i <z ; ++i) {
-        cout<<a[i]<<" ";
-    }
-    for (int i = 0; i <z ; ++i) {
-        for (int j = i+1; j <z ; ++j) {
-            if(a[i]>a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-    cout<<'\n';
+    int height, width;
+    cout<<"please inpute the width :";
+    cin>>width;
+    cout<<"Please inpute the height :";
+    cin>>height;
+    box_of_stars(width,height);
 
-    cout<<"ASCENDING ORDER : ";
-    for (int i = 0; i <z ; ++i) {
-        cout<<a[i]<<" ";
-    }
     return 0;
 }
